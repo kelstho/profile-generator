d0e554bed723e26514ecf34534b54bbb360e5787
Kelly Thompson <ksthompson48@gmail.com>
2020-02-12 16:13:50 -0600

    added electron variable

--- index.js
+++ index.js
@@ -100,6 +100,8 @@ promptUser()
         const queryUrl = `https://api.github.com/users/${username}`;
 
         axios.get(queryUrl).then(function(data){
+            console.log(data);
+
             const html = generateHTML(data);
 
             return writeFileAsync("index.html", html);
