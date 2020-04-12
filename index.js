bef4eda966c0dd9c4717ac81c81e94940e914331
Kelly Thompson <ksthompson48@gmail.com>
2020-04-12 14:24:33 -0500

    remove custom style sheets

--- index.js
+++ index.js
@@ -29,9 +29,7 @@ function generateHTML(res) {
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
       <title>${res.data.name}</title>
-      <link rel="stylesheet" type="text/css" href="assets/css/reset.css">
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-      <link rel="stylesheet" type="text/css" href="assets/css/style.css">
   </head>
   <body>
     <div class="jumbotron" style="background-color: ;">
